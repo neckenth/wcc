34bba48d1886e4d720658671adbfe9ba4e9f2f17
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-01-07 17:03:14 -0500

    2nd Commit - Practice files for printing and variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+x = 4
+y = x + 1
+
+print(x)
+print(y)
+
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+# Ariel is 30 years old now, but she will turn 31 in August.
+# Her birth stone is peridot.
+
+print('Ariel is ' + str(age) + ' years old now, but she will turn ' + str(next_age) + ' in ' + birth_month + '.')
+print('Her birth stone is ' + birth_stone + '.')
