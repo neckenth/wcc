d85bc72085e3f61f803b7f0bc47e046ab76b2926
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-01-15 14:48:07 -0500

    Commit #12 - loops practice

---
+++ loops.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# counter = 1
+# while counter <= 10:
+#     print '#' * counter
+#     counter = counter + 1
+
+# # First, define the sequence we’ll loop through
+# numbers = range(0,10)
+# # Used Built-in Python function range() to produce a list: [0,1,2,3,4,5,6,7,8,9]
+# for counter in numbers:
+#     print '#' * counter
+
+# greeting = 'Hello World'
+# for letter in greeting:
+#     print letter
+
+# continents = ['Africa', 'Antarctica', 'Asia', 'Europe', 'North America', 'Australia', 'South America']
+# for continent in continents:
+#     print continent + ' is a continent.'
+
+# print range(0, 15)
+#
+# print range(10)
+
+vowels = ['a','e', 'i', 'o', 'u']
+
+def print_vowels(vowels):
+    for vowel in vowels:
+        print vowel
+print_vowels(vowels)
