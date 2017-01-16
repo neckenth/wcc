b94d73954d8fbf1fbc531e8a127a69bebcc1653f
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-01-16 14:32:28 -0500

    Commit #15 - guess_again_v2.py - 2 new versions

--- guess_again_v2.py
+++ guess_again_v2.py
@@ -2,26 +2,18 @@ import random
 
 
 def get_guess():
-    # Get initial guess
     guess = raw_input('Enter your guess: ')
 
-    # Assume it's not valid, until it's proven otherwise
     valid = False
 
     while valid != True:
         try:
-            # Try and convert this number to an integer
-            # If it fails, the exception will occur
             guess = int(guess)
         except Exception:
-            # Exception was thrown when trying to convert to number,
-            # Report the issue and ask again
             print('Invalid input; please enter a whole number.')
             valid = False
             guess = get_guess()
 
-        # If they get here, it means the number must have been valid
-        # Set valid to be true to break out of the while loop
         valid = True
 
     return guess
@@ -34,27 +26,56 @@ def compare(num_a, num_b):
     elif num_a == num_b:
         return 'equal'
 
+# this version works, but I don't think I should need all these if...else conditions
+
 
 def play(minimum, maximum, tries):
     secret_number = random.randint(minimum, maximum)
-    # print('TEMPORARY DEBUGGING HELPER -> The secret number is: ' + str(secret_number))
+    print('TEMPORARY DEBUGGING HELPER -> The secret number is: ' + str(secret_number))
 
     print("\nI'm thinking of a number between " + str(minimum) + " and " + str(maximum) + "; what do you think it is?")
 
     for guess_count in range(tries):
         num_a = get_guess()
         num_b = secret_number
-        compare(num_a, num_b)
-        while compare(num_a, num_b) != 'equal':
-            guess_count += 1
-            if compare(num_a, num_b) == 'high':
-                print('Too ' + compare(num_a, num_b) + '. You have ' + str(tries - guess_count) + ' guesses left.')
-            elif compare(num_a, num_b) == 'low':
-                print('Too ' + compare(num_a, num_b) + '. You have ' + str(tries - guess_count) + ' guesses left.')
-            num_a = get_guess()
-        if compare(num_a, num_b) == 'equal':
+        results = compare(num_a, num_b)
+        guess_count += 1
+        if results != 'equal':
+            if guess_count != tries:
+                if results == 'high':
+                    print('Too ' + results + '. You have ' + str(tries - guess_count) + ' guesses left.')
+                elif results == 'low':
+                    print('Too ' + results + '. You have ' + str(tries - guess_count) + ' guesses left.')
+            else:
+                print('Sorry, you ran out of turns! The correct number was ' + str(secret_number))
+        else:
             print('You got it! The number was ' + str(secret_number) + '.')
             break
-    print('Sorry, you ran out of turns! The correct number was ' + str(secret_number) + '.')
+
+# alternate version - why isn't the 'out of turns' statement being reached?
+
+
+def play(minimum, maximum, tries):
+    secret_number = random.randint(minimum, maximum)
+    print('TEMPORARY DEBUGGING HELPER -> The secret number is: ' + str(secret_number))
+
+    print("\nI'm thinking of a number between " + str(minimum) + " and " + str(
+        maximum) + "; what do you think it is?")
+
+    for guess_count in range(tries):
+        num_a = get_guess()
+        num_b = secret_number
+        results = compare(num_a, num_b)
+        guess_count += 1
+        if results != 'equal':
+            if results == 'high':
+                print('Too ' + results + '. You have ' + str(tries - guess_count) + ' guesses left.')
+            elif results == 'low':
+                print('Too ' + results + '. You have ' + str(tries - guess_count) + ' guesses left.')
+            elif guess_count == tries:
+                print('Sorry, you ran out of turns! The correct number was ' + str(secret_number))
+        else:
+            print('You got it! The number was ' + str(secret_number) + '.')
+            break
 
 play(1, 20, 5)
